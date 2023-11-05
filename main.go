9326b6f5d7495fb32a4a63ffb4e6590bbae08647
ralphvw <williamsralph@enyata.com>
2023-11-05 20:15:09 +0000

    CORS on main

--- main.go
+++ main.go
@@ -20,15 +20,16 @@ func main() {
 	db := db.InitDb()
 
 	fmt.Print("Server started at " + port + "\n")
-	http.HandleFunc("/", func(http.ResponseWriter, *http.Request) {
+	http.HandleFunc("/", func(w http.ResponseWriter, _ *http.Request) {
+		helpers.EnableCors(w)
 		helpers.LogAction("Welcome")
 	})
 
 	http.HandleFunc("/auth/login", handlers.Login(db))
 	http.HandleFunc("/auth/signup", handlers.SignUp(db))
 	http.HandleFunc("/auth/send-reset-password-email", handlers.SendResetMail(db))
-  http.HandleFunc("/auth/reset-password", handlers.ResetPassword(db))
-  http.HandleFunc("/users", handlers.GetAllUsers(db))
+	http.HandleFunc("/auth/reset-password", handlers.ResetPassword(db))
+	http.HandleFunc("/users", handlers.GetAllUsers(db))
 	err := http.ListenAndServe(":"+port, nil)
 	if err != nil {
 		log.Fatal("Server error:", err)
