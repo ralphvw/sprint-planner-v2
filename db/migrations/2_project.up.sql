20055cc66cf68744bf8bf538a8adc719d7335e75
ralphvw <williamsralph@enyata.com>
2023-11-13 09:09:08 +0000

    Project migrations

---
+++ db/migrations/2_project.up.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS projects(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR,
+  description VARCHAR,
+  created_at TIMESTAMPTZ DEFAULT NOW(),
+  updated_at TIMESTAMPTZ DEFAULT NOW()
+)
---
+++ db/migrations/3_project_members.up.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS project_members(
+  id SERIAL PRIMARY KEY,
+  project_id INT REFERENCES projects(id) ON DELETE CASCADE,
+  user_id INT REFERNCES users(id) ON DELETE CASCADE,
+  created_at TIMESTAMPTZ DEFAULT NOW(),
+  updated_at TIMESTAMPTZ DEFAULT NOW()
+)
