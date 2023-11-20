16b20a7d52d2b2a055ee463dc4fe95929d574e64
ralphvw <williamsralph@enyata.com>
2023-11-20 12:04:16 +0000

    DB FIX

--- db/migrations/5_sprint_members.up.sql
+++ db/migrations/5_sprint_members.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS sprint_members(
   id SERIAL PRIMARY KEY,
   user_id INT REFERENCES users (id) ON DELETE CASCADE,
-  sprint_id INT REFERENCES sprints (id) ON DELETE CASCADE
+  sprint_id INT REFERENCES sprints (id) ON DELETE CASCADE,
   designation VARCHAR,
   created_at TIMESTAMPTZ DEFAULT NOW(),
   updated_at TIMESTAMPTZ DEFAULT NOW()
