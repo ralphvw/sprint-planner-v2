2d8002d19469f835794a2be28fe5bf57de216c1a
ralphvw <williamsralph@enyata.com>
2023-11-23 10:51:24 +0000

    Fixed users search

--- queries/user.go
+++ queries/user.go
@@ -6,4 +6,4 @@ var GetUserByEmail string = "SELECT id, email, hash, first_name, last_name FROM 
 
 var ResetPassword string = "UPDATE users SET password=$1 WHERE email=$2 RETURNING email"
 
-var SearchUsers string = "SELECT id, email, first_name, last_name FROM users WHERE email ILIKE $1 OR first_name+last_name ILIKE $1"
+var SearchUsers string = "SELECT id, email, first_name, last_name FROM users WHERE email ILIKE $1 OR CONCAT(first_name, ' ', last_name) ILIKE $1"
